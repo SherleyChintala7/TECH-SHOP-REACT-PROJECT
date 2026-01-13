24b8831e293d499f487df76d3a45dfc807dbade1
SherleyChintala7 <sherleychintala77@gmail.com>
2026-01-13 23:17:26 +0530

    nineth commit

---
+++ src/Components/Authentication/Login.jsx
@@ -0,0 +1,29 @@
+import "./Auth.css";
+
+const Login = ({ switchToSignup }) => {
+  return (
+    <>
+    <div className="auth-container">
+      <h2>Login</h2>
+
+      <p className="switch-text">
+        New to Tech-Shop?
+        <span onClick={switchToSignup}> Create an account</span>
+      </p>
+
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+
+      <button className="primary-btn">Login</button>
+    </div>
+    <p className="login"><p>__________</p><p className="login-txt">or login with</p><p>__________</p></p>
+    <div className="social-btns">
+      <button className="fb">Facebook</button>
+      <button className="google">Google</button>
+      <button className="tweet">Twitter</button>
+    </div>
+    </>
+  );
+};
+
+export default Login;
